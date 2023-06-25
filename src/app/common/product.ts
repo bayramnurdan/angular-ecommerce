b7e56e89fe7dcdfa5bc697b4e8b5cb19404b87e6
Nurdan Emin <nurdanemin0@gmail.com>
2023-06-25 14:27:59 +0300

    initial commit
    initial commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    constructor(public name: string,
+        public brandName: string,
+        public description: string,
+        public quantity: number,
+        public price: number,
+        public discount: number){}
+}
